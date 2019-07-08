b4dc11e4df26feaca9fc2ffd04c7a90565050e32
rjgates3 <gates.j.richard@gmail.com>
2019-07-08 14:23:27 -0600

    added more functions to drills2

--- drills2.js
+++ drills2.js
@@ -49,3 +49,63 @@ function decode(code) {
 }
 
 console.log(decode('craft block argon meter bells brown croon droop'));
+
+function daysInMonth(month, leapYear = false) {
+  let days;
+  switch(month) {
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    days = 31;
+    break;
+  case 'Febuary':
+    if (leapYear) {
+      days = 29;
+      break;
+    } else {
+      days = 28;
+      break;
+    }
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    days = 30;
+    break;
+  }
+  return `${month} has ${days} days`;
+}
+
+console.log(daysInMonth('January'));
+console.log(daysInMonth('Febuary', true));
+console.log(daysInMonth('Febuary'));
+console.log(daysInMonth('June'));
+
+function rockPaperScissors(num) {
+  const rNum = Math.floor(Math.random() * 3) + 1;
+
+  if (num !== 1 && num !== 2 && num !== 3) {
+    throw new Error('You must pick 1 or 2 or 3!');
+  }
+  if (num === rNum) {
+    console.log('It\'s a tie');
+  } else if ((num === 1 && rNum === 3) || (num === 2 && rNum === 1) || (num === 3 && rNum === 2)) {
+    console.log('You win!');
+  } else {
+    console.log('You lose.');
+  }
+}
+
+try {
+  rockPaperScissors(1);
+  rockPaperScissors(2);
+  rockPaperScissors(3);
+  rockPaperScissors(7);
+}
+catch(error) {
+  console.error(error);
+}
